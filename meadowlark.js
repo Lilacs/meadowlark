568a6a0c09fe8db48314ee690385d8468a1b0ff2
Lilacs <zp1990@me.com>
2016-03-01 09:12:43 +0800

    ch07 添加中间件 页面模版

--- meadowlark.js
+++ meadowlark.js
@@ -1,49 +1,106 @@
 var express = require('express');
+var fortunes = require('./lib/fortune.js');
+
 var app = express();
-var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
+var handlebars = require('express3-handlebars').create({
+    defaultLayout: 'main',
+    helpers: {
+        section: function (name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
 
-var fortunes = require('./lib/fortune.js');
-
-app.use(function(req, res, next) {
-	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
-	next();
-});
-
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-    // res.type('text/plain');
-    // res.send('Meadowlark Travel');
-    res.render('home',  { pageTestScript: '/qa/tests-global.js'});
+// set 'showTests' context property if the querystring contains test=1
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
 });
 
-app.get('/about', function(req, res) {
+// mocked weather data
+function getWeatherData() {
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
+// 添加中间件
+app.use(function (req, res, next) {
+    if (!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weatherContext = getWeatherData();
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.render('home', {pageTestScript: '/qa/tests-global.js'});
+});
+
+app.get('/about', function (req, res) {
     // res.type('text/plain');
     // res.send('About Meadowlark Travel');
-    res.render('about', { fortune: fortunes.getFortune(), pageTestScript: '/qa/tests-about.js'});
+    res.render('about', {fortune: fortunes.getFortune(), pageTestScript: '/qa/tests-about.js'});
 });
 
-app.get('/tours/hood-river', function(req, res) {
-	res.render('tours/hood-river');
+app.get('/tours/hood-river', function (req, res) {
+    res.render('tours/hood-river');
 });
 
-app.get('/tours/request-group-rate', function(req, res) {
-	res.render('tours/request-group-rate');
+app.get('/tours/request-group-rate', function (req, res) {
+    res.render('tours/request-group-rate');
 });
 
-app.use(function(req, res){
+app.get('/nursery-rhyme', function(req, res){
+    res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme', function(req, res){
+    res.json({
+        animal: 'squirrel',
+        bodyPart: 'tail',
+        adjective: 'bushy',
+        noun: 'heck',
+    });
+});
+
+app.use(function (req, res) {
     // res.type('text/plain');
     res.status(404);
     // res.send('404 - Not Found');
     res.render('404');
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     console.error(err.stack);
     // res.type('text/plain');
     res.status(500);
@@ -51,6 +108,6 @@ app.use(function(err, req, res, next) {
     res.render('500');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('Express start on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate');
 });
